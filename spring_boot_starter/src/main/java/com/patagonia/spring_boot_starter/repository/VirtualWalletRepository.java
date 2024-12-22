6185cebf50dbf83d551e25457fb4f8b5fe0861b1
xunfangce <xunfangce@gmail.com>
2024-12-22 16:53:17 +0800

    remove useless

--- spring_boot_starter/src/main/java/com/patagonia/spring_boot_starter/repository/VirtualWalletRepository.java
+++
@@ -1,8 +0,0 @@
-package com.patagonia.spring_boot_starter.repository;
-
-/**
- * @author xunfangce
- * @create 2024/11/9
- */
-public class VirtualWalletRepository {
-}
--- spring_boot_starter/src/main/java/com/patagonia/spring_boot_starter/service/VirtualWalletService.java
+++
@@ -1,8 +0,0 @@
-package com.patagonia.spring_boot_starter.service;
-
-/**
- * @author xunfangce
- * @create 2024/11/9
- */
-public interface VirtualWalletService {
-}
