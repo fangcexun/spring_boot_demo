18ccd01ef592bafebffd9f813a378bfc07279433
xunfangce <xunfangce@gmail.com>
2024-12-22 17:40:38 +0800

    added function

--- starter/src/main/java/com/patagonia/starter/SpringBootStarterApplication.java
+++ starter/src/main/java/com/patagonia/starter/SpringBootStarterApplication.java
@@ -3,7 +3,7 @@ package com.patagonia.starter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.patagonia")
+@SpringBootApplication(scanBasePackages = {"com.patagonia.controller", "com.patagonia.service"})
 public class SpringBootStarterApplication {
 
     public static void main(String[] args) {
