556a66fb97b3c9c435a58b6384b9806010546d01
xunfangce <xunfangce@gmail.com>
2025-01-02 09:57:13 +0800

    bean study

--- starter/src/main/java/com/patagonia/starter/SpringBootStarterApplication.java
+++ starter/src/main/java/com/patagonia/starter/SpringBootStarterApplication.java
@@ -1,26 +1,27 @@
 package com.patagonia.starter;
 
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication(scanBasePackages = {"com.patagonia.controller", "com.patagonia.service"})
+@SpringBootApplication(scanBasePackages = {"com.patagonia.controller", "com.patagonia.service", "com.patagonia.starter"})
 public class SpringBootStarterApplication {
 
     public static void main(String[] args) {
 
         ConfigurableApplicationContext context = SpringApplication.run(SpringBootStarterApplication.class, args);
+        ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
 
         // configurableApplicationContext 实例类型
-        System.out.println(context);
-        System.out.println(context.getClass());
+        System.out.println("容器的实现类 " + context.getClass());
 
         // beanFactory 实例类型
-        System.out.println(context.getBeanFactory().getClass());
+        System.out.println("beanFactory 的实现类 " + beanFactory.getClass());
 
         // beanDefinition 实例类型
-
-
+        System.out.println("testBeanName 的 definition 实现类 " + beanFactory.getBeanDefinition("testBeanName").getClass());
+        System.out.println("beanFunction 的 definition 实现类 " + beanFactory.getBeanDefinition("beanFunction").getClass());
     }
 
 }
--- starter/src/main/java/com/patagonia/starter/TestBean.java
+++ starter/src/main/java/com/patagonia/starter/TestBean.java
@@ -4,7 +4,6 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -12,21 +11,15 @@ import org.springframework.stereotype.Component;
  * @author xunfangce
  * @create 2024/12/29
  */
-@Component
+@Component("testBeanName") // Scanned...Definition 来创建 bean
 @Slf4j
 public class TestBean {
     // 1. 构造方法
     public TestBean() {
         System.out.println("TestBean Constructor");
-        log.info("TestBean Constructor");
     }
     // 2. 注入方法
-    @Autowired
-    public void autowireFunction() {
-        System.out.println("TestBean autowire");
-    }
-
-    @Bean
+    @Bean // Configuration...Definition 来创建 bean
     public void beanFunction() {
         System.out.println("TestBean bean");
     }
@@ -35,6 +28,10 @@ public class TestBean {
     @PostConstruct
     public void init() {
         System.out.println("TestBean init");
+    }
+
+    public void myFunction() {
+        System.out.println("TestBean myFunction");
     }
     // 4. 销毁方法
     @PreDestroy
